b5c039c950db802e86f7a096b9103bdf6fce2735
Francesco Punta <puntafrancesco@gmail.com>
2023-02-07 19:04:51 +0100

    Test

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('while')
+
+let dispari = []
+
+
+do {
+	numero = parseInt(prompt('Inserisci un numero dispari'))
+
+	if (numero % 2 == 0) {
+		console.log('Il numero inserito è pari')
+	}else{
+		numero === dispari
+		console.log
+	}
+
+
+} while ((dispari))
