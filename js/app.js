0407983fe7b5f24bbcdd77e2a20f452109732a9c
Francesco Punta <puntafrancesco@gmail.com>
2023-02-07 19:12:09 +0100

    analisi

--- js/app.js
+++ js/app.js
@@ -2,16 +2,11 @@ console.log('while')
 
 let dispari = []
 
+// Chiedo numero
 
-do {
-	numero = parseInt(prompt('Inserisci un numero dispari'))
+// se resto operazione = 0 allora richiedo, sennò inserisco il numero dispari in un array
 
-	if (numero % 2 == 0) {
-		console.log('Il numero inserito è pari')
-	}else{
-		numero === dispari
-		console.log
-	}
+// ripeto questa operazione finchè i numeri dispari non sono 10
 
+//li stampo uno alla volta in console
 
-} while ((dispari))
